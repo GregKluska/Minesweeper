529218898f50e46ad80de825d0006cf5236217fa
Greg Kluska <kluskagreg@gmail.com>
2021-10-21 22:49:57 +0100

    Simplify board layout

--- app/src/main/java/com/gregkluska/minesweeper/components/BoardLayout.kt
+++ app/src/main/java/com/gregkluska/minesweeper/components/BoardLayout.kt
@@ -1,6 +1,5 @@
 package com.gregkluska.minesweeper.components
 
-import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.tooling.preview.Preview
@@ -9,31 +8,15 @@ import com.gregkluska.minesweeper.Options
 import com.gregkluska.minesweeper.ui.theme.Green
 import com.gregkluska.minesweeper.ui.theme.GreenLight
 
-interface BoardScope {
-    fun field(content: @Composable () -> Unit)
-}
-
-private data class BoardItem(
-    val content: @Composable () -> Unit
+data class BoardItem(
+    val component: @Composable () -> Unit
 )
-
-private class BoardScopeImpl : BoardScope {
-
-    val fieldsss: MutableList<BoardItem> = mutableListOf()
-
-    override fun field(content: @Composable () -> Unit) {
-        fieldsss.add(element = BoardItem(content))
-    }
-
-}
 
 @Composable
 fun Board(
     options: Options = Options(),
-    fields: BoardScope.() -> Unit
+    fields: List<BoardItem>
 ) {
-
-    val items = BoardScopeImpl().apply { fields() }
 
     SubcomposeLayout { constraints ->
         val layoutWidth = constraints.maxWidth
@@ -55,8 +38,8 @@ fun Board(
             var r: Int = 0
             var c: Int = 0
 
-            items.fieldsss.forEachIndexed { index, item ->
-                val minefieldPlaceables = subcompose(index, item.content).map {
+            fields.forEachIndexed { index, item ->
+                val minefieldPlaceables = subcompose(index, item.component).map {
                     it.measure(minefieldConstraints)
                 }
 
@@ -83,27 +66,33 @@ fun Board(
 @Preview
 @Composable
 private fun BoardPreview() {
-
+    val fields = mutableListOf<BoardItem>()
     val options = Options(columns = 10)
-    Board(
-        options = options
-    ) {
-        for(i in 0 until 100) {
-            // Todo: Think of a better way of getting colors.
-            val row = ( i.floorDiv(options.columns) )
 
-            val color1 = if(i.rem(2) > 0) Green else GreenLight
-            val color2 = if(i.rem(2) > 0) GreenLight else Green
+    for(i in 0 until 100) {
+        // Todo: Think of a better way of getting colors.
+        val row = ( i.floorDiv(options.columns) )
 
-            val color = if(options.columns.rem(2)>0) color1 else if(row.rem(2) > 0) color1 else color2
+        val color1 = if(i.rem(2) > 0) Green else GreenLight
+        val color2 = if(i.rem(2) > 0) GreenLight else Green
 
-            field {
-                Field(
-                    color = color
-                )
-            }
+        val color = if(options.columns.rem(2)>0) color1 else if(row.rem(2) > 0) color1 else color2
+
+        fields.add (
+            BoardItem(
+                component = {
+                    Field(
+                        color = color
+                    )
+                }
+            )
+        )
 
 
-        }
     }
+
+    Board(
+        options = options,
+        fields = fields
+    )
 }
